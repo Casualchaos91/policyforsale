45fb3c1ca411850a222bd44274c337cab1ea5bcc
Casualchaos91 <dreamneedle91@aol.com>
2025-02-17 16:22:52 -0600

    Add files via upload

---
+++ script(9).js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var ctx = document.getElementById('donationChart').getContext('2d');
+    var donationChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Private Prisons', 'Alcohol & Tobacco', 'Anti-Marijuana', 'Big Pharma', 'Fossil Fuels'],
+            datasets: [{
+                label: 'Millions USD',
+                data: [12, 8.5, 6.5, 10, 14],
+                backgroundColor: ['blue', 'red', 'green', 'purple', 'orange']
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+});
+
+function copyToClipboard(button) {
+    var input = button.previousElementSibling;
+    input.select();
+    document.execCommand("copy");
+    button.innerText = "Copied!";
+    setTimeout(() => button.innerText = "Copy", 1000);
+}
